75978de23d11509b5b511b9dcd7f439dd0b19d9a
Chethan M <46964474+iam-chethan-m@users.noreply.github.com>
2026-08-07 22:34:54 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+          return ""
+        
+        encoded_strs = []
+        for s in strs:
+            if s == "":
+                encoded_strs.append(":")
+            else:
+                encoded_strs.append("#".join(str(ord(c)) for c in s))
+        # print(encoded_strs)
+
+        return "|".join(encoded_strs)
+
+    def decode(self, s: str) -> List[str]:
+        
+        if s=="":
+            return []
+        strs = s.split("|")
+        decode_strs = []
+        print(strs)
+        for s in strs:
+            if s==":":
+                decode_strs.append("")
+            else:
+                word = "".join(chr(int(x)) for x in s.split("#"))
+                decode_strs.append(word)
+        return decode_strs
